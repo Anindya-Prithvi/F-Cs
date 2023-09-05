91aa72c73391167b7dab80305e688067207a9504
vm <xxxx>
2023-09-05 19:12:36 +0000

    frontend; react router/nav page style

---
+++ newapp/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>
+                Login area
+            </h1>
+        </div>
+    );
+};
+
+export default Login;
