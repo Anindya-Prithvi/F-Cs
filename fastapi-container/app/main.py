cf2de6d5c3ad03c2e432ddfe917cb928ab1cb031
Anindya Prithvi <anindya20024@iiitd.ac.in>
2023-09-14 08:36:24 +0000

    everything https

--- fastapi-container/app/main.py
+++ fastapi-container/app/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from app.routers.login import router as login_router
 from pymongo import MongoClient
+import time
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+app.add_middleware(HTTPSRedirectMiddleware)
+
 mongo_client = None
 app.include_router(login_router)
 
