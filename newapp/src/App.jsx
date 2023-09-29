0f351c6493c130dfa610d091c6cc25333b699f0c
Anindya Prithvi <anindya20024@iiitd.ac.in>
2023-09-29 18:34:10 +0000

    React=>Vite

---
+++ newapp/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count sjis {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <pre className="text-3xl font-bold underline">
+        Hello world!
+      </pre>
+    </>
+  )
+}
+
+export default App
