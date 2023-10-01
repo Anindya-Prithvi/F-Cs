4cc5871b033142084864fd4677c7477f7f502401
Anindya Prithvi <anindya20024@iiitd.ac.in>
2023-10-01 17:20:39 +0000

    Whazever

--- newapp/src/pages/Home.jsx
+++ newapp/src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Navbar from "../components/header/Navbar"
+import axios_api from '../utilities/axios';
 
 const Home = () => {
+    const dummyreq = (e) => {
+        // e.preventDefault();
+        axios_api.get("/status")
+            .then(function (response) {
+                console.log("[DEBUG]" + response);
+            }).catch(function (error) {
+                console.log("[DEBUG] recv error: ", error)
+            });
+    }
     return (
-        <div className='w-screen h-screen'>
-            <Navbar />
-        </div>
+        <>
+            <div className='w-screen h-screen'>
+                <Navbar />
+                <button type='button' className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full" onClick={dummyreq}>
+                    RED BUTTON!
+                </button>
+            </div>
+        </>
+
     )
 }
 
--- newapp/src/utilities/axios.jsx
+++ newapp/src/utilities/axios.jsx
@@ -3,43 +3,47 @@ import { showAlert } from "../utilities/toast"
 
 export const baseUrl = process.env.APIloc || "https://192.168.2.233:8000"
 export const baseApi = baseUrl + "/api/v1"
+export const setLoggedOut = () => {
+    localStorage.clear()
+    sessionStorage.clear()
+    location.assign("/login")
+}
 
 const axios_api = axios_base.create({
     baseURL: baseApi
 })
 
 if (sessionStorage.getItem("token"))
-    axios_api.defaults.headers.common['Authorization'] = "Token " + sessionStorage.getItem("token");
+    axios_api.defaults.headers.common['Authorization'] = "Bearer " + sessionStorage.getItem("token");
 
 export const setToken = (access_token) => {
-    axios_api.defaults.headers.common['Authorization'] = "Token " + access_token;
+    axios_api.defaults.headers.common['Authorization'] = "Bearer " + access_token;
     sessionStorage.setItem("token", access_token)
 }
 
-// axios_api.interceptors.response.use(function (response) {
-//     return response;
-// }, function (error) {
-//     console.log("[DEBUG], got triggered" + error)
+axios_api.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    console.log("[DEBUG], got triggered" + error)
 
-//     if (error.response.status === 500)
-//         showAlert(
-//             "Unexpected error occurred. Please contact us if you see this message repeatedly.",
-//             "error"
-//         )
-//     else if (error.response.status === 401) {
-//         showAlert(
-//             "Authentication error",
-//             "error"
-//         )
-//         //TODO: set logout
-//         setLoggedOut()
-//     }
-//     else {
-//         console.log("[DEBUG]; some error occured" + error)
-//         showAlert("See for yourself: " + error.response.data["detail"], "error")
-//     }
+    if (error.response.status === 500)
+        showAlert(
+            "Unexpected error occurred. Please contact us if you see this message repeatedly.",
+            "error"
+        )
+    else if (error.response.status === 401) {
+        showAlert(
+            "Authentication error. Please relogin",
+            "error"
+        )
+        setLoggedOut()
+    }
+    else {
+        console.log("[DEBUG] some error occured:" + error)
+        throw error;
+    }
 
-//     return Promise.reject(error);
-// });
+    return Promise.reject(error);
+});
 
 export default axios_api
