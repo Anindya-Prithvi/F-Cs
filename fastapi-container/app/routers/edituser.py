f31ca9f650b0701cde42904cef752e5332b7e1ad
Anindya Prithvi <anindya20024@iiitd.ac.in>
2023-10-03 14:23:29 +0000

    Added edit profile api

--- fastapi-container/app/routers/edituser.py
+++ fastapi-container/app/routers/edituser.py
@@ -1,14 +1,36 @@
-from .login import User, get_current_active_user, LOGIN_CREDENTIALS_COLLECTION
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+
+from .login import LOGIN_CREDENTIALS_COLLECTION, User, get_current_active_user, pwd_context, verify_password
 
 router = APIRouter(prefix="/api/v1", tags=["update-api"])
 
-class update_user(BaseModel):
+
+class UpdateUser(BaseModel):
+    password: str # mandatory
     email: str | None = None
     full_name: str | None = None
+    new_password: str | None = None
+
 
 @router.post("/users/me/update")
-async def update_user_info(update_user: update_user, current_user: User = Depends(get_current_active_user)):
-    LOGIN_CREDENTIALS_COLLECTION.find_one_and_update({"username": current_user.username}, { "$set": {"email": update_user.email, "full_name": update_user.full_name} })
+async def update_user_info(
+    update_user: UpdateUser, current_user: User = Depends(get_current_active_user)
+):
+    pwd_hash = pwd_context.hash(update_user.password)
+    npwd_hash = None
+    if update_user.new_password is not None:
+        npwd_hash = pwd_context.hash(update_user.new_password)
+    
+    pwd_hash_of_user = LOGIN_CREDENTIALS_COLLECTION.find_one({"username": current_user.username}, projection={"hashed_password"})
+    verified = verify_password(update_user.password, pwd_hash_of_user['hashed_password'])
+
+    if verified:
+        rval = LOGIN_CREDENTIALS_COLLECTION.find_one_and_update(
+            {"username": current_user.username, "hashed_password": pwd_hash_of_user['hashed_password']},
+            {"$set": {"email": update_user.email or current_user.email, "full_name": update_user.full_name or current_user.full_name, "hashed_password": npwd_hash or pwd_hash}},
+        )
+        print(rval)
+    else:
+        raise HTTPException(status_code=401, detail="Wrong Password")
     return {"detail": "ok"}
